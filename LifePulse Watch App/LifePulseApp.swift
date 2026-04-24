18a2b18718df4dda8392ad2cd90380852825e8b2
trevorlink <lvpidadiao@hotmail.com>
2026-04-24 11:46:16 +0800

    Initial Commit

---
+++ LifePulse Watch App/LifePulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifePulseApp.swift
+//  LifePulse Watch App
+//
+//  Created by trevorlink on 2026/4/24.
+//
+
+import SwiftUI
+
+@main
+struct LifePulse_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LifePulse/LifePulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifePulseApp.swift
+//  LifePulse
+//
+//  Created by trevorlink on 2026/4/24.
+//
+
+import SwiftUI
+
+@main
+struct LifePulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
