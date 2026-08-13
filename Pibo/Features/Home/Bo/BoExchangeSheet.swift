2aa51f2815d4267bad0057260801225b8b064752
trevorlink <lvpidadiao@hotmail.com>
2026-08-14 04:06:23 +0800

    refactor: extract ornament unlock motion

--- Pibo/Features/Home/Bo/BoExchangeSheet.swift
+++ Pibo/Features/Home/Bo/BoExchangeSheet.swift
@@ -795,29 +795,6 @@ struct BoUnlockOverlay: View {
     }
 }
 
-enum OrnamentUnlockMotion {
-    static let materializationMilliseconds: Int64 = 1_120
-    static let flightMilliseconds: Int64 = 280
-    static let layerMilliseconds: Int64 = 200
-    static let confirmationMilliseconds: Int64 = 240
-    static let easingBezier = [0.22, 1.0, 0.36, 1.0]
-
-    static let materializationDuration: Duration = .milliseconds(materializationMilliseconds)
-    static let flightDuration = Double(flightMilliseconds) / 1_000
-    static let layerDuration = Double(layerMilliseconds) / 1_000
-    static let confirmationDuration = Double(confirmationMilliseconds) / 1_000
-
-    static func constructionAnimation(duration: Double) -> Animation {
-        .timingCurve(
-            easingBezier[0],
-            easingBezier[1],
-            easingBezier[2],
-            easingBezier[3],
-            duration: duration
-        )
-    }
-}
-
 struct OrnamentArtwork: View {
     let ornament: PiboOrnament
     let locked: Bool
